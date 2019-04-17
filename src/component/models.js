8b014ed314d1dc88660361346bff97e4504c4b87
Shirley940408 <Shirley940408@gmail.com>
2019-04-16 22:07:26 -0400

    move token to localStorage

--- src/component/models.js
+++ src/component/models.js
@@ -1,79 +1,79 @@
-import {SERVER_ADDRESS} from '../constants';
+import { SERVER_ADDRESS } from '../constants';
 import axios from 'axios';
 export const questions = {
-    state: [],
-    reducers:{
-        set(state,payload){
-            return payload;
-        }
-    },
-    effects:dispatch=>({
-        getAll(payload,state){
-          callAPI({
-          method: 'get',
-          uri: '/questions',
-          }).then(
-            response=>{
-              dispatch.questions.set(response.data.questions);
-            }
-          )
-        },
-        create(payload, state){
-          console.log(payload);
-          if(!state.user_token){
-            alert("You have not login!");
-            return;
-          }
-          callAPI({
-            method: 'post',
-            uri: '/questions',
-            headers:{
-              'Authorization': JSON.stringify({
-                user_token:{
-                  user_id: state.user_token.user_id,
-                  key: state.user_token.key,
-                }
-              }),
-            },
-            data: {
-              question: {
-                title: payload.title,
-                content:payload.content,
-              }
-            },
-            errHandler: status => status == 400,
-          }).then(
-            response =>{
-              if(response.status == 201 ){
-                payload.success_callback && payload.success_callback();
-                dispatch.questions.getAll();
-                // this.setState({should_redirect: true});
-              }else if(response.status == 400){
-                let error_first = response.data.errors[0];
-                if(error_first.code == 'duplicated_field'){
-                  alert("The email has been registered!");
-                }else{
-                  alert("Unexpected error happened, please contact lalala@gmail.com");
-                }
-              }
-            }
-          )
-        }
-    }),
-}
-
-export const user_token = {
-  state: null,
-  reducers:{
-    set(state,payload){
+  state: [],
+  reducers: {
+    set(state, payload) {
       return payload;
     }
   },
-  effects: dispatch=>({
-    create(payload,state){
+  effects: dispatch => ({
+    getAll(payload, state) {
       callAPI({
-        uri:'/user_tokens',
-        method:'post',
+        method: 'get',
+        uri: '/questions',
+      }).then(
+        response => {
+          dispatch.questions.set(response.data.questions);
+        }
+      )
+    },
+    create(payload, state) {
+      console.log(payload);
+      if (!state.user_token) {
+        alert("You have not login!");
+        return;
+      }
+      callAPI({
+        method: 'post',
+        uri: '/questions',
+        headers: {
+          'Authorization': JSON.stringify({
+            user_token: {
+              user_id: state.user_token.user_id,
+              key: state.user_token.key,
+            }
+          }),
+        },
+        data: {
+          question: {
+            title: payload.title,
+            content: payload.content,
+          }
+        },
+        errHandler: status => status == 400,
+      }).then(
+        response => {
+          if (response.status == 201) {
+            payload.success_callback && payload.success_callback();
+            dispatch.questions.getAll();
+            // this.setState({should_redirect: true});
+          } else if (response.status == 400) {
+            let error_first = response.data.errors[0];
+            if (error_first.code == 'duplicated_field') {
+              alert("The email has been registered!");
+            } else {
+              alert("Unexpected error happened, please contact lalala@gmail.com");
+            }
+          }
+        }
+      )
+    }
+  }),
+}
+
+export const user_token = {
+  state: JSON.parse(localStorage.getItem('user_token')),
+  reducers: {
+    set(state, payload) {
+      return payload;
+    }
+  },
+  effects: dispatch => ({
+    create(payload, state) {
+      callAPI({
+        uri: '/user_tokens',
+        method: 'post',
         data: {
           credential: {
             email: payload.email,
@@ -83,14 +83,15 @@ export const user_token = {
         errHandler: status => status == 400
       }).then(
         response => {
-          if(response.status == 201) {
+          if (response.status == 201) {
             dispatch.user_token.set(response.data.user_token);
+            localStorage.setItem('user_token', JSON.stringify(response.data.user_token));
             payload.success_callback && payload.success_callback();
-          }else if(response.status == 400){
+          } else if (response.status == 400) {
             let error_first = response.data.errors[0];
-            if(error_first.code == 'invalid_credential'){
+            if (error_first.code == 'invalid_credential') {
               alert('Email or password is wrong!');
-            }else{
+            } else {
               alert('Unexpected error happened, please contact lalala@gmail.com');
             }
           }
@@ -101,19 +102,19 @@ export const user_token = {
 }
 
 
-export const users={
+export const users = {
   state: {},
-  reducers:{
-    set(state,payload){
-      let state_new = {...state, [payload.id]: payload };
+  reducers: {
+    set(state, payload) {
+      let state_new = { ...state, [payload.id]: payload };
       return state_new;//need to be written as pure function
     }
   },
-  effects: dispatch =>({
-    create(payload,state){
+  effects: dispatch => ({
+    create(payload, state) {
       callAPI({
         method: 'post',
-        uri:'/users',
+        uri: '/users',
         data: {
           user: {
             email: payload.email,
@@ -124,38 +125,38 @@ export const users={
         errHandler: status => status == 400,
       }).then(
         response => {
-          if(response.status == 201 ){
+          if (response.status == 201) {
             alert("register successful!");
             payload.success_callback && payload.success_callback();
             dispatch.users.set(response.data.user);
-          }else if(response.status == 400){
+          } else if (response.status == 400) {
             let error_first = response.data.errors[0];
-            if(error_first.code == 'duplicated_field'){
+            if (error_first.code == 'duplicated_field') {
               alert("The email has been registered!");
-            }else{
+            } else {
               alert("Unexpected error happened, please contact lalala@gmail.com");
             }
           }
         }
       )
-    }    
+    }
   })
 }
-function callAPI({uri, method='get', data, errHandler=()=>false, headers,}){
+function callAPI({ uri, method = 'get', data, errHandler = () => false, headers, }) {
   const request = axios({
     method,
-    url : SERVER_ADDRESS + uri,
+    url: SERVER_ADDRESS + uri,
     data,
     headers,
-    validateStatus: (status) =>{
-      if((status >=200 && status < 300) || errHandler(status) ){
+    validateStatus: (status) => {
+      if ((status >= 200 && status < 300) || errHandler(status)) {
         return true;
-      }else{
+      } else {
         return false;
       }
     }
   });
-  return request.catch((error)=>{
+  return request.catch((error) => {
     alert("Unexpected error happened, please contact lalala@gmail.com")
   });
 }
