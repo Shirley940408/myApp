7f6ed2aedcafed72afe324465e966fee443c8553
Shirley940408 <Shirley940408@gmail.com>
2019-04-27 01:41:18 -0400

    add styles of User page

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import UserFetcher from '../component/UserFetcher';
-
+import styles from './styles/User';
+import defaultAvatar from '../imgs/avatar_default.jpg'
 export default class User extends Component{
   
   render(){
     const { id }= this.props.match.params;
     console.log(id);
     return(
-      <div style={{width:50, height:50, backgroundColor:'red'}}>
+      <div style={styles.container}>
       <UserFetcher id={id}>
       {user=>{
         return(
-          <div>
-            {JSON.stringify(user)}
+          <div style={styles.panel_container}>
+            <img style={styles.avatar_image} src={user.avatar_url||defaultAvatar} alt=""/>
+            <div style={styles.info_container}>
+              
+            </div>
           </div>
         )
       }}
---
+++ src/pages/styles/User.js
@@ -0,0 +1,34 @@
+const styles = {
+  container:{
+    width:'100vw',
+  },
+  panel_container:{
+    //styling self
+    marginLeft:'auto',
+    marginRight:'auto',
+    width: 1200,
+    background:'white',
+    marginTop:56,
+    //styling children
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'flex-start',
+    alignItems:'center',
+  },
+  info_container:{
+    //styling children
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'flex-start',
+    alignItems:'center',
+  },
+  avatar_image:{
+    width:250,
+    height:250,
+    marginLeft:30,
+    // marginTop:-36,
+    position:'relative',
+    top:-36,
+  }
+}
+export default styles;
